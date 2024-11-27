7f58ae8fa84ceced084900879a4a860c210262c7
Gangula-Varshitha-Reddy <96340672+Gangula-Varshitha@users.noreply.github.com>
2024-11-27 13:46:07 -0500

    Update auth.service.ts

--- Mineral-Insights-Frontend/src/app/services/auth.service.ts
+++ Mineral-Insights-Frontend/src/app/services/auth.service.ts
@@ -6,7 +6,7 @@ import { Observable, BehaviorSubject } from 'rxjs';
   providedIn: 'root'
 })
 export class AuthService {
-  private baseUrl = '/api'; // Point to the backend with a proxy or direct path
+  private baseUrl = 'http://157.230.190.101:3000'; // Point to the backend with a proxy or direct path
   private tokenKey = 'jwtToken';
   private nameKey = 'name';
   private isLoggedInSubject = new BehaviorSubject<boolean>(this.isLoggedIn());
